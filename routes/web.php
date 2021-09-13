ebdab0f3f0ce7c062ed03f6be2d6d7b174109834
Diskaoktavaia <diskaoktavia36@gmail.com>
2021-09-13 11:19:37 +0700

    membuat HomeController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticlesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,10 +18,17 @@ use App\Http\Controllers\PageController;
 
 Route::get('/', function () {
     return view('welcome');
+});
 
 Route::get('/', [PageController::class, 'index']);
 
 Route::get('/about', [PageController::class,'about']);
 
 Route::get('/articles/{id}', [PageController::class, 'articles']);
-});
+
+
+/* membuat single Controller */
+
+Route::get('/', [HomeController::class, 'index']);
+
+
